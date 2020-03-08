18b2534930cc98202c1f5e36e1dac5b8ad06c4a6
Konnichiwaeien <konnichiwaeien@gmail.com>
2020-03-08 10:16:16 +0500

    singolo part3

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+var fields = document.querySelectorAll(".form__field");
+var capts = document.querySelectorAll(".form__capt");
+
+var focusField = function(item, capt) {
+  item.addEventListener("focus", function() {
+    capt.classList.add("form__capt--full");
+  });
+};
+
+var blurField = function(item, capt) {
+  item.addEventListener("blur", function() {
+    if(!(item.value)) {
+      capt.classList.remove("form__capt--full");
+    }
+  });
+};
+
+var interSearch = function () {
+  for(var i = 0; i < fields.length; i++) {
+    if(!(fields[i].value)) {
+      focusField(fields[i], capts[i]);
+      blurField(fields[i], capts[i]);
+    }
+  }
+};
+
+interSearch();
+
+
